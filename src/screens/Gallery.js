2c622d50b49765146c0c9f66af1221cfecc7862a
Valentyn Koval <koval.valentyn.pavlovich@gmail.com>
2017-07-07 13:44:20 +0300

    add main screen

--- src/screens/Gallery.js
+++ src/screens/Gallery.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, ScrollView, Button, View } from 'react-native';
+import {connect} from 'react-redux';
+import axios from 'axios';
+import * as data from '../redux/actions/dataActions.js';
+import * as page from '../redux/actions/pageActions.js';
+import ElementGallery from '../components/ElementGallery.js';
+// import ListGallery from '../components/ListGallery.js';
 
-export default class Gallery extends React.Component {
+
+class Gallery extends React.Component {
   componentDidMount(){
     if(this.props.page == 1){
         axios.get('https://api.500px.com/v1/photos?feature=popular&consumer_key=wB4ozJxTijCwNuggJvPGtBGCRqaZVcF6jsrzUadF&page=1')
       .then((response) => {
+          // console.log('response',response);
+          // console.log('response');
           this.props.dispatch(page.edit(2));
           this.props.dispatch(data.create(response.data.photos));
       })
@@ -14,22 +23,39 @@ export default class Gallery extends React.Component {
       });
     }
   }
+
   render() {
-
+    // console.log('text');
+    if(this.props.data){
+       // console.log('data ', this.props.data.data.photos);
+        this.list =  this.props.data.map((e, i) => {
+          // console.log(e.user);
+           return ( <ElementGallery data={e} idImage={i} key={i}/> );
+        });
+    }
     return (
-      <View style={styles.container}>
-        <Text>Its gallery grid!</Text>
-
-      </View>
+      <ScrollView>
+        <View style={style.content}>
+            {this.list}
+        </View>
+        <Button  onPress={() => this.props.navigation.navigate('Image', { user: 'Lucy' })} title="Chat with Lucy" />
+      </ScrollView>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const style = StyleSheet.create({
+    content:{
+      flexWrap: 'wrap',
+      display: 'flex',
+      justifyContent: 'center',
+      flexDirection: 'row'
+    }
 });
+function mapStateToProps(state){
+  return {
+    data : state.dataGallery,
+    page : state.page
+  }
+};
+
+export default connect(mapStateToProps)(Gallery);
